4f9d456a00839440dd18405cfc49d0eb26e9f2e9
keioni <devel@keioni.com>
2024-04-22 16:40:45 +0900

    minor fix

--- bin/upload.sh
+++ bin/upload.sh
@@ -6,7 +6,7 @@ tarfile=/tmp/$server.tar.gz
 cat www/img/connected/via_ipv4.png | bin/binary-to-array/binary-to-array ipv4_img > bin/ipaddr-logger/ipv4_img_array.h
 cat www/img/connected/via_ipv6.png | bin/binary-to-array/binary-to-array ipv6_img > bin/ipaddr-logger/ipv6_img_array.h
 
-cp -pr bin/ipaddr-logger www/cgi-bin/src
+cp -pr bin/ipaddr-logger/* www/cgi-bin/src/
 
 tar zcvf $tarfile \
     --no-mac-metadata \
