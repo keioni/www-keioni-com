1867d08ee555a8158328634a0657fe761ad5d30b
keioni <devel@keioni.com>
2024-04-22 17:16:31 +0900

    add error handling

--- bin/upload.sh
+++ bin/upload.sh
@@ -3,6 +3,10 @@
 server=keioni.com
 tarfile=/tmp/$server.tar.gz
 
+if [ ! -x bin/binary-to-array/binary-to-array ]; then
+    make -C bin/binary-to-array
+fi
+
 cat www/img/connected/via_ipv4.png | bin/binary-to-array/binary-to-array ipv4_img > bin/ipaddr-logger/ipv4_img_array.h
 cat www/img/connected/via_ipv6.png | bin/binary-to-array/binary-to-array ipv6_img > bin/ipaddr-logger/ipv6_img_array.h
 
@@ -13,7 +17,7 @@ tar zcvf $tarfile \
     --exclude .DS_Store \
     --exclude '*.log' \
     --exclude '.git*' \
-    --exclude '.o' \
+    --exclude '*.o' \
     -C www .
 
 scp $tarfile bin/server-side-deploy.sh keioni.com:
