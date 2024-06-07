64e57d37da87f994de5a82b72b47a005d1a95420
keioni <devel@keioni.com>
2024-06-07 12:37:28 +0900

    update uploader

--- bin/upload.sh
+++ bin/upload.sh
@@ -7,6 +7,7 @@ if [ ! -x bin/binary-to-array/binary-to-array ]; then
     make -C bin/binary-to-array
 fi
 
+mkdir -p bin/ipaddr-logger
 cat www/img/connected/via_ipv4.png | bin/binary-to-array/binary-to-array ipv4_img > bin/ipaddr-logger/ipv4_img_array.h
 cat www/img/connected/via_ipv6.png | bin/binary-to-array/binary-to-array ipv6_img > bin/ipaddr-logger/ipv6_img_array.h
 
