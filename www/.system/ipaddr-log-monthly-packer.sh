29639fbbeca5301d164d5900ce3e3834cfa78b74
keioni <devel@keioni.com>
2024-06-18 10:30:07 +0900

    change aggregate method

--- www/.system/ipaddr-log-monthly-packer.sh
+++ www/.system/ipaddr-log-monthly-packer.sh
@@ -3,10 +3,7 @@
 target_month=`date -v-1m "+%Y%m"`
 
 cd $LOG_DIR
-mkdir -p $target_month
+cat ${target_month}*.log > aggregate_${target_month}.log
+# rm ${target_month}*.log
 
-mv ${target_month}*.log $target_month
-
-tar zcf ${target_month}.tar.gz $target_month
-
-rm -rf $target_month
+mv aggregate_${target_month}.log ${target_month}.log
